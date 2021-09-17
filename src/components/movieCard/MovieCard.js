5f0017c49582bbacbc723a1c4c30e16a2966422c
farish <developer.websites1@gmail.com>
2021-09-17 16:53:44 +0530

    Fetching data from API

---
+++ src/components/movieCard/MovieCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MovieCard({id, title, poster, date,media,vote}) {
+    return (
+        <div>
+            {title}
+        </div>
+    )
+}
+
+export default MovieCard
--- src/components/pages/Trending.js
+++ src/components/pages/Trending.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React,{useState , useEffect} from 'react';
+import MovieCard from '../movieCard/MovieCard';
 
 function Trending() {
+
+    const [content, setContent] = useState([]);
+
+    const fetchData = async() =>{
+       const url =
+      " https://api.themoviedb.org/3/trending/all/day?api_key=f3356f78904a7602215ab2fccacb4356";
+       const data = await fetch(url);
+       const parsedData = await data.json()
+    //    console.log(parsedData);
+       setContent(parsedData.results)
+    }
+
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return (
         <div>
-            Trending
+            <span className="pageTitle">Trending</span>
+            <div className="trending">
+                {
+                    content && content.map((element) =>
+                        <MovieCard id ={element.id} title={element.title || element.name} poster={element.poster_path} date={element.first_air_date || element.release_date} media={element.media_type} vote={element.vote_average}/>
+                        // console.log(element)
+                    )
+                }
+            </div>
         </div>
     )
 }
